d3572dcfedbc0dc6e8f00c318a066bbe306beb96
Jane Suh <sourcherryyogurt@gmail.com>
2020-10-12 12:21:28 -0400

    Initial commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,3 +1,24 @@
+"""
+# Merging Two Packages
+
+Given a package with a weight limit `limit` and a list `weights` of item
+weights, implement a function `get_indices_of_item_weights` that finds
+two items whose sum of weights equals the weight limit `limit`. Your
+function will return a tuple of integers that has the following form:
+
+```
+(zero, one)
+```
+
+where each element represents the item weights of the two packages.
+_**The higher valued index should be placed in the `zeroth` index and
+the smaller index should be placed in the `first` index.**_ If such a
+pair doesn’t exist for the given inputs, your function should return
+`None`.
+
+Your solution should run in linear time.
+"""
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
