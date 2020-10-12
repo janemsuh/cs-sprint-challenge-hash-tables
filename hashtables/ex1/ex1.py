499359327527472f1eecba23beeac323618ed515
Jane Suh <sourcherryyogurt@gmail.com>
2020-10-12 12:29:29 -0400

    Completed ex1. Merging Two Packages

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -23,6 +23,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # create dict
+    weight_dict = dict()
+    
+    # weights as key, weight list index as value
+    for i in range(0, length):
+        weight_dict[weights[i]] = i
+
+    # loop through weights
+    for i in range(0, length):
+        if limit - weights[i] in weight_dict:
+            weight_index1 = i
+            weight_index2 = weight_dict[limit - weights[i]]
+            # check list index order
+            if weight_index1 < weight_index2:
+                weight_index1, weight_index2 = weight_index2, weight_index1
+            return weight_index1, weight_index2
 
     return None
+
+weights_test = [ 4, 6, 10, 15, 16 ]
+print(get_indices_of_item_weights(weights_test, 5, 21))
+# output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
